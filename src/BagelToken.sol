8adb5b8362ea82128b363eb22ce3f43b21bdc530
Shivang <dixitshiva12358@gmail.com>
2025-04-19 03:32:58 +0530

    First

---
+++ src/BagelToken.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzepplin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzepplin/contracts/access/Ownable.sol";
+
+
+contract BagelToken is ERC20, Ownable {
+    constructor() ERC20("BagelToken", "BGL") {}
+
+    function mint(address to , uint256 amount ) external onlyOwner {
+        _mint(to , amount);
+    }
+
+}
